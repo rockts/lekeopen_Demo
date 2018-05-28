8f11689aca7c7b1a2349999b7d617fa48fee06e5
rockts <rockts@sina.com>
2018-05-28 22:05:36 +0800

    请求登录凭证（code）

--- pages/event/event.js
+++ pages/event/event.js
@@ -1,17 +1,24 @@
 Page({
-  onLoad() {
-    console.log('页面加载');
-  },
-  onShow() {
-    console.log('页面显示');
-  },
-  onReady() {
-    console.log('页面首次渲染完成');
-  },
-  onHide() {
-    console.log('页面隐藏');
-  },
-  onUnload() {
-    console.log('页面卸载');
-  }
+  pay () {
+  /**
+   * 请求登录凭证。
+   */
+   wx.login({
+     success: (loginResponse) => {
+       /**
+        * 请求支付参数。
+        */
+        wx.request({
+          url: 'https://sandbox.lekee.cc/checkout/pay',
+          method: 'POST',
+          data: {
+            code: loginResponse.code
+          },
+          success: (paramResponse) => {
+            console.log(paramResponse)
+          }
+        })
+     }
+   })
+ }
 })
