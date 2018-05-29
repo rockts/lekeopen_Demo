cf81662f004778b0ebb044eb4b415a9585bfd0d8
rockts <rockts@sina.com>
2018-05-29 21:24:55 +0800

    小程序支付

--- pages/event/event.js
+++ pages/event/event.js
@@ -1,24 +1,47 @@
 Page({
   pay () {
-  /**
-   * 请求登录凭证。
-   */
-   wx.login({
-     success: (loginResponse) => {
-       /**
-        * 请求支付参数。
-        */
+    /**
+     * 请求登录凭证。
+     */
+    wx.login({
+      success: (loginResponse) => {
+        /**
+         * 请求支付参数。
+         */
         wx.request({
           url: 'https://sandbox.lekee.cc/checkout/pay',
           method: 'POST',
           data: {
             code: loginResponse.code
           },
-          success: (paramResponse) => {
-            console.log(paramResponse)
+          success: (paramsResponse) => {
+            console.log(paramsResponse)
+            const params = paramsResponse.data
+
+            /**
+             * 请求支付。
+             */
+            wx.requestPayment({
+              ...params,
+              success: (paymentResponse) => {
+                if (paymentResponse.errMsg === 'requestPayment:ok') {
+                  wx.showToast({
+                    title: '支付成功',
+                    icon: 'success',
+                    duration: 2000
+                  })
+
+                  setTimeout(() => {
+                    wx.switchTab({
+                      url: '/pages/index/index'
+                    })
+                  }, 2000)
+                }
+              }
+            })
           }
         })
-     }
-   })
- }
+      }
+    })
+  }
 })
